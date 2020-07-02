19aa4ffb00267a265dde4b44a8da7f923111c5bc
Hayato Clarke <hayatoclarke@gmail.com>
2020-07-02 21:01:28 +0900

    front end bootstrap done, seeding done

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -40,6 +40,6 @@ class FlatsController < ApplicationController
   private
 
   def flat_params
-    params.require('flat').permit(:name, :address, :description, :price_per_night, :number_of_guest)
+    params.require('flat').permit(:name, :address, :description, :price_per_night, :number_of_guests
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Clearing database...'
+Flat.destroy_all
+puts 'Database cleared.'
+
+puts 'Creating new seeds...'
+10.times do
+  flat = Flat.new(
+    name: Faker::Company.catch_phrase,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    description: "#{Faker::Appliance.brand} #{Faker::Appliance.equipment}",
+    price_per_night: rand(14..30) * 5,
+    number_of_guests: rand(1..5)
+  )
+  flat.save
+end
+puts 'Seeds created. Have a nice day.'
